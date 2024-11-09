47ea98b1b704a0b68e257b83169ea94974ec7219
mishuk09 <mishukinfo09@gmail.com>
2024-11-09 21:06:54 +0530

    update code

--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -27,7 +27,7 @@ export default function CheckoutForm({ dpmCheckerLink }) {
             elements,
             confirmParams: {
                 // Make sure to change this to your payment completion page
-                return_url: "http://localhost:3000/complete",
+                return_url: "http://localhost:3001/complete",
             },
         });
 
